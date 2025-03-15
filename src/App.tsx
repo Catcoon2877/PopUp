78084b88fca87d8788dbe688219c0f0beeb802e4
Catcoon2877 <p.mescheryakov2877@gmail.com>
2025-03-15 17:07:17 +0300

    Modal with out css

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css'
 
 function App () {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loanAmount, setLoanAmount] = useState('');
+  const [loanTerm, setLoanTerm] = useState('');
+  const [monthlyPayment, setMonthlyPlayment] = useState<number | null>(null);
+
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -10,6 +14,21 @@ function App () {
 
   const closeModal = () => {
     setIsModalOpen(false);
+    setLoanAmount('');
+    setLoanTerm('');
+    setMonthlyPlayment(null);
+  };
+
+  const calculatePayment = () => {
+    const amount = parseFloat (loanAmount);
+    const term = parseFloat(loanTerm);
+
+    if (amount && term) {
+      const monthly = amount / term;
+      setMonthlyPlayment(monthly);
+    } else {
+      alert('Пожалуйста, введите корректные данные');
+    }
   };
 
   return (
@@ -21,12 +40,45 @@ function App () {
       {isModalOpen && (
         <div className="modal-overlay">
           <div className="modal">
-          <h2> Платеж по кредиту</h2>
-          <p>Введите сумму кредита и выберете срок, на который вы хотите его оформить.</p>
-          <p>Мы автоматически рассчитаем для вас ежемесячный платеж, чтобы вы могли лучше спланировать свои финансы.</p>
-          <input></input>
-          <button></button>
-          <button onClick={closeModal}>Закрыть</button>
+            <h2> Платеж по кредиту</h2>
+            <p>Введите сумму кредита и выберете срок, на который вы хотите его оформить.</p>
+            
+            <p>Мы автоматически рассчитаем для вас ежемесячный платеж, чтобы вы могли лучше спланировать свои финансы.</p>
+
+            <div className="input-group">
+             <label>Ваша сумма кредита</label>
+              <input
+              type="number"
+              placeholder="Введите сумму"
+              value={loanAmount}
+              onChange={(e) => setLoanAmount(e.target.value)}
+          />
+          </div>
+
+          <div className="input-group">
+            <label> Количество месяцев?</label>
+            <div className="term-button">
+            <button onClick={()=> setLoanTerm('12')}>12</button>
+            <button onClick={()=> setLoanTerm('24')}>24</button>
+            <button onClick={()=> setLoanTerm('36')}>36</button>
+            <button onClick={()=> setLoanTerm('48')}>48</button>
+            </div>
+          </div>
+
+          <button className="calculate-button" onClick={calculatePayment}>
+            Рассчитать
+          </button>
+
+          {monthlyPayment !== null && (
+            <div className="result">
+              <h3>Ежемесячный платеж</h3>
+              <p>{monthlyPayment.toFixed(2)} ₽</p>
+              </div>
+          )}
+
+          <button className="close-button" onClick={closeModal}>
+            Закрыть
+            </button>   
           </div>
         </div> 
       )}
